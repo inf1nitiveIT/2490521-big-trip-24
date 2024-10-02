5d4e67e4fb0e7a0fb4efcc0fb253da5e8951bbbe
Андрей Крамарчук <inf1nitiveit@gmail.com>
2024-10-02 18:51:53 +0300

    Реализует задание 6.9

--- src/mock/destinations.js
+++ src/mock/destinations.js
@@ -3,7 +3,7 @@ import { Cities } from '../const';
 const mockDestinations = [
   {
     id: '1',
-    description: 'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.',
+    description: '',
     name: Cities.TOKIO,
     pictures: []
   },
