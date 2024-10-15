b97bb9c0bbfa3bd985dd17dc832908d152308aa4
Андрей Крамарчук <inf1nitiveit@gmail.com>
2024-10-15 10:54:20 +0300

    Правки

--- src/const.js
+++ src/const.js
@@ -59,6 +59,7 @@ const UpdateType = {
   PATCH: 'PATCH',
   MINOR: 'MINOR',
   MAJOR: 'MAJOR',
+  INIT: 'INIT',
 };
 
 const MessageTextType = {
--- src/utils.js
+++ src/utils.js
@@ -92,4 +92,16 @@ const sorting = {
   }
 };
 
-export {getRandomArrayElement, humanizeTaskDueDate, getRandomNumber, getRandomBoolean, getDifferenceTime, capitalizedFirstLetterOfString, filter, updateItem, sorting, getPointsByDate, getPointsByPrice, getPointsByTime, isDatesEqual};
+const showErrorMessage = (message) => {
+  const element = document.createElement('div');
+  element.style.width = `${100}%`;
+  element.style.height = `${3}rem`;
+  element.style.backgroundColor = 'red';
+  element.style.fontSize = `${2}rem`;
+  element.style.color = 'white';
+  element.style.textAlign = 'center';
+  element.textContent = `Error loading ${message}...`;
+  document.body.append(element);
+};
+
+export {getRandomArrayElement, humanizeTaskDueDate, getRandomNumber, getRandomBoolean, getDifferenceTime, capitalizedFirstLetterOfString, filter, updateItem, sorting, getPointsByDate, getPointsByPrice, getPointsByTime, isDatesEqual, showErrorMessage};
